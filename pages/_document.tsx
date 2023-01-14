7b3fc4c48ef27ab9126197e96a920f4f31903f18
Brett Galvez <brettjosef.galvez@cit.edu>
2023-01-15 02:12:52 +0800

    another one

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <head>
+        <title>Galvez Resume</title>
+      </head>
       <Head/>
       <body>
         <Main />
